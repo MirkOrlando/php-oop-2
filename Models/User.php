4334378eae9b819f9b1c38a94ebfb97505af4363
MirkOrlando <mirko@orlando-m.it>
2022-05-26 17:19:01 +0200

    create methods signIn and setCreditCard

--- Models/User.php
+++ Models/User.php
@@ -5,16 +5,33 @@ class User
     protected $lastname;
     protected $email;
     protected $password;
-    protected $is_logged;
+    protected $is_signed = false;
     protected $credit_card;
 
-    function __construct(String $firstname, String $lastname, String $email, String $password, bool $is_logged, array $credit_card)
+    function __construct(String $firstname, String $lastname)
     {
         $this->firstname = $firstname;
         $this->lastname = $lastname;
+    }
+
+    public function getName()
+    {
+        return $this->firstname;
+    }
+
+    public function signIn($email, $password)
+    {
         $this->email = $email;
         $this->password = $password;
-        $this->is_logged = $is_logged;
-        $this->credit_card = $credit_card;
+        $this->is_signed = true;
+    }
+
+    public function setCreditCard($card_number, $key_number, $expiration_date)
+    {
+        $this->credit_card = [
+            'number' => $card_number,
+            'key' => $key_number,
+            'expiration date' => $expiration_date,
+        ];
     }
 }
