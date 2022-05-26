6dc6de379ebeeb025bc8982dd6f110b589925863
MirkOrlando <mirko@orlando-m.it>
2022-05-26 14:56:29 +0200

    add AnimalProduct's constructor method

--- Models/AnimalProducts.php
+++ Models/AnimalProducts.php
@@ -6,4 +6,13 @@ class AnimalProducts
     protected $price;
     protected $discount;
     protected $available;
+
+    function __construct(String $name, String $description, Float $price, Int $discount, bool $available,)
+    {
+        $this->name = $name;
+        $this->description = $description;
+        $this->price = $price;
+        $this->discount = $discount;
+        $this->available = $available;
+    }
 }
