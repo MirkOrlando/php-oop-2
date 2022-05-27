04827d9cee0818965e564e918843a8d8ee5c7848
MirkOrlando <mirko@orlando-m.it>
2022-05-27 16:37:17 +0200

    add two products

--- db.php
+++ db.php
@@ -12,7 +12,9 @@ require_once __DIR__ . '/Models/Payment.php';
 $data = [
     "products" => [
         new Feed('Biscottini al formaggio', 'Lorem ipsum dolor sit amet.', 'cani', 5.99, true),
+        new Feed('Mousse all\'anatra', 'Lorem ipsum dolor sit amet.', 'cani', 5.99, true),
         new Forniture('Albero tiragraffi', 'Lorem ipsum dolor sit amet.', 'gatti', 29.99, true),
+        new Forniture('Cuccia per cani', 'Lorem ipsum dolor sit amet.', 'gatti', 29.99, true),
         new Accessory('Pettorina da passeggio', 'Lorem ipsum dolor sit amet.', 'cani', 14.99, true),
         new Healthcare('Collare antiparassitario', 'Lorem ipsum dolor sit amet.', 'gatto', 19.99, true),
     ],
